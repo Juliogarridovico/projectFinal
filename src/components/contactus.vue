9bba44f37a6e4d8bcdf89994d6fe1b3d8664dfdf
gema-munoz <gema.m.moscoso@gmail.com>
2022-10-12 22:17:05 +0200

    añadido footer y contact us

--- src/components/contactus.vue
+++ src/components/contactus.vue
@@ -1,20 +1,164 @@
 <template>
-     <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-
-   contactus
+  
+    <div class="form-area">
+      <div class="container">
+        <div class="single-form">
+          <div class="col-sm-12 col-lg-6">
+            <div class="left">
+              <h2>
+                <span
+                  >Get in touch </span
+                ><br />with TRYLLO 
+                <br> 
+              </h2>
+            </div>
+          </div>
+          <div class="col-sm-12 col-lg-6" id="right">
+            <div class="right">
+              <i class="bi bi-caret-left-fill"></i>
+              <form id="myForm">
+                <div class="mb-3">
+                  <label for="exampleInputText1" class="form-label"
+                    >Your Name*</label
+                  >
+                  <input
+                    type="text"
+                    class="form-control rounded-pill"
+                    id="exampleInputText1"
+                  />
+                </div>
+                <div class="mb-3">
+                  <label for="exampleInputEmail1" class="form-label"
+                    >Email*</label
+                  >
+                  <input
+                    type="email"
+                    class="form-control rounded-pill"
+                    id="exampleInputEmail1"
+                    aria-describedby="emailHelp"
+                    validate="required:true"
+                    pattern="[a-zA-Z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*"
+                  />
+                </div>
+                <div class="mb-3">
+                  <label for="exampleInputText1" class="form-label"
+                    >Message</label
+                  >
+                  <textarea
+                    class="form-control"
+                    id="exampleInputText1"
+                  ></textarea>
+                </div>
+                <button type="submit" class="btn btn-success rounded-pill ms-1">
+                  SUBMIT
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <hr>
+  
 </template>
 <script>
 
+// const myForm = document.getElementById("myForm");
+//       myForm.addEventListener("submit", (e) => {
+//         e.preventDefault();
+
+//        alert("Form has been submitted!");
+//       });
 </script>
+
 <style scoped>
-*{
-      color: black;
+body{
+    background: #ffffff;
+}
+
+.form-area{
+    padding-top: 7%;
+    margin-bottom: 50px;
+}
+
+.row.single-form{
+    box-shadow: 0 2px 20px -5px rgba(0, 0, 0, 0.5);
+}
+
+.left{
+    background: linear-gradient(to right, #1EC49C , #65B6F7);
+    padding: 200px 93px;
+}
+
+
+.left h2{
+    color: #ffffff;
+    font-weight: 700;
+    font-size: 48px;
+    
+}
+
+.left h2 span{
+    font-weight: 100;
+}
+
+.right{
+    padding: 70px 100px;
+    position: relative;
+    
+
+}
+
+.right i{
+    position: absolute;
+    font-size: 80px;
+    left: -12px;
+    top: 40%;
+    color: #ffffff;
+}
+
+.form-control{
+    border: 2px solid #1EC49C;
+    
+}
+#right{
+  margin-left: 620px;
+  margin-top: -500px;
+}
+
+
+.right  button{
+    border: none;
+    background:  #1EC49C;
+    width: 160px;
+    color: #ffffff;
+    padding: 15px 0;
+    display: inline-block;
+    font-size: 16px;
+    margin-top: 20px;
+    margin-right: 40%;
+    cursor: pointer;
+}
+
+.right button:hover{
+    background:  #65B6F7;
+}
+
+@media (min-width: 500px) and (max-width: 991px){
+    .right i{
+        top: -70px;
+        margin-left: -25px;
+        transform: rotate(90deg);
+        left: 50%;
     }
+}
+
+@media (min-width: 747px) and (max-width: 991px){
+    .left h2{
+        margin-left: 23%;
+    }
+}
+
+
+
 </style>
--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,15 +1,134 @@
 <template>
-<div class="cuadrado">
-  footer
-</div>
+  <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css"
+    />
+<footer>
+      <div
+        class="row justify-content-around text-center text-md-start"
+        id="footer-container"
+      >
+      <hr>
 
+        <div class="class col-md-2 text-center">
+          <img
+            class="mt-5"
+            src="../assets/images/logo_expand.svg"
+            width="70"
+            height="70"
+          />
+          <h4 class="fw-bold ps-3 text-white">TRYLLO</h4>
+        </div>
+        <div class="col-md-2">
+          <ul class="list-unstyled">
+            <li class="fw-bold my-2 text-white">ABOUT TRYLLO</li>
+            <hr class="md-3 text-white">
+            <li>
+              <p class="text-white">What's tryllo ?</p>
+            </li>
+            <li>
+              <p class="text-white"><i class="bi bi-code text-white"></i> An easy place to create and share tasks with your colleagues! <i class="bi bi-code-slash text-white"></i> </p>
+            </li>
+          </ul>
+        </div>
+        <div class="col-md-2 ">
+          <ul class="list-unstyled">
+            <li class="fw-bold my-2 text-white"> FOUNDERS </li>
+            <hr class="mb-3 text-white">
+            <li id="linkdirecto">
+              <a href="#" class="text-decoration-none text-white">Julio </a>
+            </li>
+            <li id="linkdirecto">
+              <a href="#" class="text-decoration-none text-white"> Robert </a> 
+            </li>
+            <li id="linkdirecto">
+              <a href="#" class="text-decoration-none text-white" > Gema </a>
+            </li>
+          </ul>
+        </div>
+        <div class="col-md-2 col-lg-2 col-xl-2">
+          <ul class="list-unstyled" >
+            <li class="fw-bold my-2 text-white"  id="footertitles">CONTACT</li>
+            <hr class="mb-3 text-white">
+            <li id="linkdirecto">
+              <a href="https://goo.gl/maps/YRfuvQP2xiYkkSrv6" class="text-decoration-none text-white" ><i class="bi bi-house-fill text-white"></i> IronHack, BCN 08018, ES</a>
+            </li>
+            <li>
+              <p class="text-white"><i class="bi bi-envelope-heart-fill text-white"></i> tryllo@contact.com </p>
+            </li>
+            <li>
+              <p class="text-white"><i class="bi bi-telephone-inbound-fill text-white"></i> +34 625418221</p>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <br class="mb-5">
+          <div class="text-center">
+            <p class="text-white">&copy; 2022 Copyright <a href="#" class="text-white text-decoration-none"> All rights reserved </a> </p>
+            <ul
+            class="list-unstyled list-inline"
+          >
+            <li class="list-inline-item" id="socialmedia" >
+              <a
+                href="#"
+                class="btn-floating btn-sm text-white"
+                style="font-size: 20px"
+              >
+                <i class="bi bi-discord me-1 fa-1x text-white"></i>
+              </a>
+            </li>
+            <li class="list-inline-item" id="socialmedia">
+              <a
+                href="https://github.com/TrylloTheCat"
+                class="btn-floating btn-sm text-white"
+                style="font-size: 20px"
+              >
+                <i class="bi bi-github mx-1 fa-1x text-white"></i>
+              </a>
+            </li>
+            <li class="list-inline-item" id="socialmedia" >
+              <a
+                href="twitter.com/tryllothecat"
+                class="btn-floating btn-sm text-white"
+                style="font-size: 20px"
+              >
+                <i class="bi bi-twitter mx-1 fa-1x text-white"></i>
+              </a>
+            </li>
+            <li class="list-inline-item" id="socialmedia">
+              <a
+                href="https://www.linkedin.com/in/tryllo-the-cat-947594253/?trk=public-profile-join-page"
+                class="btn-floating btn-sm"
+                style="font-size: 20px"
+              >
+                <i class="bi bi-linkedin mx-1 fa-1x text-white"></i>
+              </a>
+            </li>
+          </ul>
+          <hr class="text-white">
+          </div>
+        </div>
+
+      </div>
+    </footer>
 </template>
 <script>
 </script>
-<style>
-  .cuadrado{
-    width: 100%;
-    height: 50px;
-    background-color: blue;
-  }
+<style scoped>
+
+
+ footer{
+    background-image: linear-gradient(to right, #1EC49C , #65B6F7);
+    color: #ffffff;
+}
+
+#socialmedia :hover {
+ color:#327566 !important;
+}
+
+
+ #linkdirecto a:hover{
+    border-bottom: 2px solid #ffffff;
+    color: #ffffff !important;
+ } 
 </style>
