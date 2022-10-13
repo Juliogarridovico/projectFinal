662acca76ae2e3a80ad507ca89d609ecd5d9ee3b
yobo-16 <robert.anibal.ledezma@gmail.com>
2022-10-13 19:18:17 +0200

    Ajustado errores

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -5,7 +5,7 @@
       <div class="col-md-5 p-lg-5 mx-auto my-5">
         <h1 class="display-4 font-weight-normal">Team #10</h1>
         <p class="lead font-weight-normal">Le presentamos nuestro proyecto</p>
-        <a class="btn btn-outline-light rounded-5" href="/auth" target="_blank">Your Porfile</a>
+        <a class="btn btn-outline-light rounded-5" href="/profile" >Your Porfile</a>
       </div>
   </div>
 
